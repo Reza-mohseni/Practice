dc507087d71b9e13976e576065ff07e1277275a4
Reza Mohseni <mohseni.rezaofficial@gmail.com>
2023-04-13 17:24:44 +0330

    The first 10 sessions

---
+++ the-first-10-sessions.php
@@ -0,0 +1,17 @@
+<?php
+//
+$arr= array("red","blue","white");
+//var_dump($arr);
+//echo implode("/",$arr);
+$number="10000000";
+//echo number_format($number,2,".","/");
+$str="hi Im RezaMohseni my job is programming";
+$str1="hi Im RezaMohseni my job is game programming game";
+//echo str_replace("programming","developer",$str);
+//var_dump(str_split($str));
+//?
+//echo strcmp($str1,$str);
+//?
+//echo strlen($str);
+//echo strpos($str1,"my");
+echo substr($str,16);
